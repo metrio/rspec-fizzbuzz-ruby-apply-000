32f264e8aaf3b87d57425761a989195df6ff6cd2
Demetrio Lima <dlima.nyit@gmail.com>
2020-08-18 19:12:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,13 +3,13 @@
 
 def fizzbuzz(int)
   
-  if int % 3 == 0
+  if int % 3 == 
     puts "Fizz"
       
-  elsif int %  5 == 0
+  elsif int %  5 == 
     puts "Buzz"
       
-  elsif int % 15 == 0
+  elsif int % 15 == 
     puts "Fizzbuzz"
       
   elsif

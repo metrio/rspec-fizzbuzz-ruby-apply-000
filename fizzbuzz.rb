bd6eb8ca72c817176b5117a90b7fef61b2ea4c88
Demetrio Lima <dlima.nyit@gmail.com>
2020-08-18 18:59:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,18 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(int)
+  
+  if fizzbuzz(int) % 3 == 3 
+    puts "Fizz"
+      
+  elsif fizzbuzz(int) %  5 == 5
+    puts "Buzz"
+      
+  elsif fizzbuzz(int) % 15 == 15
+    puts "Fizzbuzz"
+      
+  elsif
+    puts nil
+  end
+end
